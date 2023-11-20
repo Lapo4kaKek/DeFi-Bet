47861832009ea61d12ef4a7f0477a68b1df56f95
Lapo4kaKek <Sheydos2003@yandex.ru>
2023-11-21 00:51:10 +0300

    add implementation send_transaction and get_Nonce

--- src/BlockchainClient.rs
+++ src/BlockchainClient.rs
@@ -1,29 +1,69 @@
-struct BlockchainClient {
+use web3::Error;
+use web3::ethabi::Bytes;
+
+use web3::{
+    ethabi::ethereum_types::U256,
+    types::{Address, TransactionRequest},
+};
+use web3::transports::Http;
+
+pub struct BlockchainClient {
     rpc: String,
     private_key: String,
     account_address: String,
+    web3: web3::Web3<Http>
 }
 impl BlockchainClient {
-    fn new(rpc_url: String, key: String, address: String) -> Self {
-        BlockchainClient {rpc: rpc_url, private_key: key, account_address: address}
-    }
+    pub fn new(rpc_url: String, key: String, address: String) -> Self {
+        let http = web3::transports::Http::new(&rpc_url).expect("Failed to create HTTP transport");
+        let web3 = web3::Web3::new(http);
 
-    fn send_transaction(&self, function_name: String) {
-
+        BlockchainClient {
+            rpc: rpc_url,
+            private_key: key,
+            account_address: address,
+            web3
+        }
     }
-    fn send_transfer(&self, function_name: String) {
-
-    }
-    fn send_call(&self, function_name: String) {
-
-    }
-    fn get_nonce(&self) {
+    pub fn send_transfer(&self) {
 
     }
-    fn get_estimationfee(&self) {
+    pub fn send_call(&self, function_name: String) {
 
     }
-    fn get_balance(&self) {
+    pub async fn get_nonce(&self, address: web3::types::Address) -> web3::Result<u64> {
+        self.web3.eth().transaction_count(address, None).await;
+    }
+    pub fn get_estimationfee(&self) {
 
+    }
+    pub fn get_balance(&self) {
+
+    }
+    pub async fn send_transaction(&self, from: web3::types::Address, to: web3::types::Address,
+                                  value: web3::types::U256, data: Bytes, private_key: &str, function_name: String)
+    -> web3::Result<web3::types::H256> {
+        let nonce = self.get_nonce(from).await?;
+
+        // Build the tx object
+        let transaction = TransactionRequest {
+            from,
+            to: Some(to),
+            gas: None,
+            nonce: Some(nonce),
+            value: Some(value),
+            data: Some(data.into()),
+            ..Default::default()
+        };
+        // sign transaction
+        let signature =
+            self.web3.accounts().sign_transaction(transaction, &private_key.parse().unwrap()).await?;
+
+        // send transaction
+        let result = self.web3.eth().send_raw_transaction(signature.raw_transaction).await;
+
+        println!("Tx succeeded with hash: {}", result);
+
+        Ok(())
     }
 }
