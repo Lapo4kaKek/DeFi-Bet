8b4d07045807df46c6260ece6605c615ee68b7e3
Lapo4kaKek <Sheydos2003@yandex.ru>
2023-11-25 19:10:47 +0300

    add settle bets and fix a few bags

--- Contract/Storage.sol
+++ Contract/Storage.sol
@@ -9,7 +9,7 @@ contract Storage {
     mapping (uint => Event) public events;
     uint public eventCount;
 
-    addres public owner;
+    address immutable owner;
     // fixing the contract creator
     constructor() {
         owner = msg.sender;
@@ -19,12 +19,12 @@ contract Storage {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
-    event AddUser(address Address);
+    event UserAdded(address Address);
 
     function AddUser() public {
         require(users[msg.sender].balance == 0, "User already exists");
-        users[msg.sender] = new User(msg.balance, true, 0);
-        emit AddUser(msg.sender);
+        users[msg.sender] = User(address(msg.sender).balance, true, 0);
+        emit UserAdded(msg.sender);
     }
 
     function addEvent(string memory title) public {
@@ -37,29 +37,29 @@ contract Storage {
         eventCount++;
     }
 
-    function placeBet(uint betId, bool betOnYes, uint amount) public returns (string status) {
+    function placeBet(uint betId, bool betOnYes, uint amount) public returns (string memory status) {
         // check betId
         require(events[betId].isActive == false, "Incorrect event");
-
         // check user balance
         require("Not enough Balance", msg.balance < amount);
 
         // if he bet "yes"
         if(betOnYes) {
-            events[eventId].yesBets.push(Bet({user: msg.sender, amount: amount}));
+            events[betId].yesBets.push(Bet({user: msg.sender, amount: amount}));
+            return "Succes!";
         } else {
-            events[eventId].noBets.push(Bet({user: msg.sender, amount: amount}));
+            events[betId].noBets.push(Bet({user: msg.sender, amount: amount}));
+            return "Succes!";
         }
     }
 
-    function calculateExodus(uint betId) public returns (){
+    function calculateExodus(uint betId) public returns (uint256 result){
         uint sumYes = sumPick(betId, true);
         uint sumNo = sumPick(betId, false);
-
-
-
+        uint256 res = 1;
+        return res;
     }
-    function sumPick(uint betId, bool yes) {
+    function sumPick(uint betId, bool yes) public onlyOwner returns(uint256 result){
         uint amount = 0;
         if (yes) {
             for (uint i = 0; i < events[betId].yesBets.length(); i++) {
@@ -75,5 +75,22 @@ contract Storage {
 
     function settleBets(uint betId, bool outcomeYes) public onlyOwner {
         // we should calculate exodus event
+        require(events[betId].isActive, "The event is not active");
+
+        uint totalBetsYes = sumPick(betId, true); 
+        uint totalBetsNo = sumPick(betId, false);
+        uint totalBets = totalBetsYes + totalBetsNo;
+
+        if (outcomeYes) {
+            for(int i = 0; i < events[betId].yesBets.length(); i++) {
+                uint amount_out = (events[betId].yesBets[i].amount / totalBetsNo) * totalBets;
+                users[events[betId].noBets[i].userAddress].balance += amount_out;
+            }
+        } else {
+            for(int i = 0; i < events[betId].noBets.length(); i++) {
+                uint amount_out = (events[betId].noBets[i].amount / totalBetsNo) * totalBets;
+                users[events[betId].noBets[i].userAddress].balance += amount_out;
+            }
+        }
     }
 }
